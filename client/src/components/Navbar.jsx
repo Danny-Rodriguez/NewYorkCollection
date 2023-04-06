1f465a3565784b8235e697a8ed3f706adf4dfa14
Danny-Rodriguez <3039433+Danny-Rodriguez@users.noreply.github.com>
2023-04-05 17:38:39 -0700

    Tweaked logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 // import { useSelector } from "react-redux"
 
 function Navbar({ cart, getCartTotal }) {
@@ -8,8 +8,11 @@ function Navbar({ cart, getCartTotal }) {
       {/* <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 shadow-sm"> */}
       <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
         <div className="container-fluid">
-          <NavLink className="navbar-brand fw-bold fs-4 brand" to="/">
-            NEW YORK COLLECTION
+          <NavLink className="navbar-brand fw-bold fs-5 brand" to="/">
+            NEW YORK COLLECTION{" "}
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag-fill mb-2" viewBox="0 0 16 16">
+              <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z" />
+            </svg>
           </NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -54,7 +57,7 @@ function Navbar({ cart, getCartTotal }) {
         </div>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
